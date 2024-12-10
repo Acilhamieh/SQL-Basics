394a6a5e51040efa225d01f0b20fd5d4cd0c4043
Acil <acilhamieh4@gmail.com>
2024-12-10 20:41:40 +0200

    basics queries

---
+++ answers.sh
@@ -0,0 +1,14 @@
+//1-
+select name from students 
+//2-
+select * from students where age > 30 
+//3-
+select name from students where Gender='F' and age=30
+//4-
+select points from students where name='Alex'
+//5-
+insert into students VALUES(7,"Acil",21,"F",400)
+//6-
+update students set Points=Points+100 where name="Basma"
+//7-
+update students set Points=Points-50 where name="Alex"
